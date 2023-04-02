28a35e473285b8b932a83dd25f24d51a1692028a
Alvin Mithamo <alvinmithamo100@gmail.com>
2023-04-02 14:42:50 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :properties
   resources :tenants
   resources :bookings
-  resources :properties, only: [:index]
+  resources :users, only: [:create]
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/current_user', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
